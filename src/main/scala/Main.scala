52b7fb64ed37b510d8c4acbaf22ca9e64a64c78b
Amir <natural6t@gmail.com>
2024-05-24 01:13:06 +0200

    Add demonstration of basic commands in the main method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,28 @@
 object Main extends App{
-  val spiral = new SpiralMatrix(10)
+  // creating a spiral matrix with side size 5
+  val spiral = new SpiralMatrix(5)
+
+  println("printing a matrix without adjusting")
+  spiral.printMatrix()
+
+  println("printing a matrix with adjusting")
   spiral.printMatrix(true)
+
+  println("printing a matrix without adjusting with a custom separator")
+  spiral.printMatrix(separator = "-")
+
+  println("printing a matrix with adjusting, custom separator and a custom adjustChar")
+  spiral.printMatrix(true, '-', "-")
+
+  println("changing a size of a spiral matrix")
+  spiral.setSize(2)
+  spiral.printMatrix(true)
+
+  println("iterating trough a matrix")
+  for (i <- 0 until spiral.getSize()) {
+    for (j  <- 0 until spiral.getSize()) {
+      println(s"Matrix($i)($j) = ${spiral(i)(j)}")
+    }
+  }
+
 }
