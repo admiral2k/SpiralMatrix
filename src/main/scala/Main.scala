f77f5022016fc7d3305e2a31785deb9a89ce07b0
Amir <natural6t@gmail.com>
2024-05-24 00:06:34 +0200

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+object Main extends App{
+  print(1)
+  val spiral = new SpiralMatrix(5)
+  print(2)
+  spiral.printMatrix()
+}
---
+++ src/main/scala/SpiralMatrics.scala
@@ -0,0 +1,61 @@
+class SpiralMatrix(sideSize: Int){
+  private var matrix = makeSpiral(sideSize)
+
+  def toArray(): Array[Array[Int]] = matrix
+
+  def printMatrix(adjustSize: Boolean = false, adjustChar: Char = ' '): Unit = {
+    var amountOfAdjustments = 0
+    for (i <- 0 until sideSize){
+      for (j <- 0 until sideSize){
+        if (adjustSize) {
+          amountOfAdjustments = (sideSize*sideSize).toString.length - (matrix(i)(j).toString.length)
+        }
+        println(s" res${adjustChar * amountOfAdjustments}res")
+        println(adjustChar * amountOfAdjustments + matrix(i)(j) + adjustChar)
+      }
+    }
+  }
+
+  private def makeSpiral(n: Int): Array[Array[Int]] = {
+    var leftBorderX = 0
+    var rightBorderX = n - 1
+    var topBorderY = 0
+    var bottomBorderY = n - 1
+    var x = 0
+    var y = 0
+    var direction = 0
+    var value = 1
+    val matrix = Array.ofDim[Int](n, n)
+    while (value <= n * n ){
+      //println(s"v=$value x=$x y=$y d=$direction")
+      matrix(x)(y) = value
+      value += 1
+      if (direction == 0) {
+        x += 1
+        if (x == rightBorderX) {
+          direction = 1
+          topBorderY += 1
+        }
+      } else if (direction == 1) {
+        y += 1
+        if (y == bottomBorderY){
+          direction = 2
+          rightBorderX -= 1
+        }
+      } else if (direction == 2){
+        x -= 1
+        if (x == leftBorderX) {
+          direction = 3
+          bottomBorderY -= 1
+        }
+      } else if (direction == 3) {
+        y -= 1
+        if (y == topBorderY){
+          direction = 0
+          leftBorderX += 1
+        }
+      }
+    }
+    matrix
+  }
+}
